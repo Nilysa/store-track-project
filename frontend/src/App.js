34fdbd7d41cbb513718d464c09fc7b9c8257583e
nilysa <niloufar.asoubar@gmail.com>
2025-08-14 11:26:46 +0330

    new pictures added for products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
 
 function App() {
@@ -27,82 +27,31 @@ function App() {
                 <button className="sidebar-close-button" onClick={closeMenu}>x</button>
                 <ul>
                     <li>
-                        <a href="index.html">paintings</a>
+                        <a href="index.html">Paintings</a>
                     </li>
                     <li>
-                        <a href="index.html">posters</a>
+                        <a href="index.html">Posters</a>
                     </li>
                 </ul>
             </aside>
             <main className="main">
                 <div className="content">
                     <ul className="products">
-                        <li>
+                      {
+                        data.products.map(product => 
+                          <li>
                             <div className="product">
-                                <img className="product-image" src="/images/picture1.jpg" alt="product"></img>
+                                <img className="product-image" src={product.image} alt="product"></img>
                                 <div className="product-name">
-                                    <a href="product.html">Maanalaiset / Subterraneans</a>
+                                    <a href="product.html">{product.name}</a>
                                 </div>
-                                <div className="product-artist">Timo Sälekivi</div>
-                                <div className="product-price">$100</div>
-                                <div className="product-rating">4.5 stars(8 reviews)</div>
+                                <div className="product-artist">{product.artist}</div>
+                                <div className="product-price">${product.price}</div>
+                                <div className="product-rating">{product.rating} stars({product.numReviews} reviews)</div>
                             </div>
                         </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/picture1.jpg" alt="product"></img>
-                                <div className="product-name">
-                                    <a href="product.html">Maanalaiset / Subterraneans</a>
-                                </div>
-                                <div className="product-artist">Timo Sälekivi</div>
-                                <div className="product-price">$100</div>
-                                <div className="product-rating">4.5 stars(8 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/picture1.jpg" alt="product"></img>
-                                <div className="product-name">
-                                    <a href="product.html">Maanalaiset / Subterraneans</a>
-                                </div>
-                                <div className="product-artist">Timo Sälekivi</div>
-                                <div className="product-price">$100</div>
-                                <div className="product-rating">4.5 stars(8 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/picture1.jpg" alt="product"></img>
-                                <div className="product-name">
-                                    <a href="product.html">Maanalaiset / Subterraneans</a>
-                                </div>
-                                <div className="product-artist">Timo Sälekivi</div>
-                                <div className="product-price">$100</div>
-                                <div className="product-rating">4.5 stars(8 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/picture1.jpg" alt="product"></img>
-                                <div className="product-name">
-                                    <a href="product.html">Maanalaiset / Subterraneans</a>
-                                </div>
-                                <div className="product-artist">Timo Sälekivi</div>
-                                <div className="product-price">$100</div>
-                                <div className="product-rating">4.5 stars(8 reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="/images/picture1.jpg" alt="product"></img>
-                                <div className="product-name">
-                                    <a href="product.html">Maanalaiset / Subterraneans</a>
-                                </div>
-                                <div className="product-artist">Timo Sälekivi</div>
-                                <div className="product-price">$100</div>
-                                <div className="product-rating">4.5 stars(8 reviews)</div>
-                            </div>
-                        </li>
+                        )
+                      }
                     </ul>
                 </div>
             </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default{
+    products: [
+        {
+        name: 'Maanalaiset / Subterraneans',
+        category: 'Paintings',
+        image: '/images/picture1.jpg',
+        price: 100,
+        artist:'Timo Sälekivi',
+        rating: 4,
+        numReviews: 8
+        },
+        {
+        name: 'pomegranate feast',
+        category: 'Paintings',
+        image: '/images/picture2.jpg',
+        price: 90,
+        artist:'Monika mostowik',
+        rating: 5,
+        numReviews: 10
+        },
+        {
+        name: 'Fall',
+        category: 'Posters',
+        image: '/images/picture3.jpg',
+        price: 50,
+        artist:'Jason Chan',
+        rating: 4.5,
+        numReviews: 20
+        }
+    ]
+}
